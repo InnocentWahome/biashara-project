326382fbec27e4d91aef159d62305e9a10b70269
InnocentWahome <innocentwahome@gmail.com>
2022-02-01 00:37:11 +0300

    auth and basic crud set

--- server/app/Models/User.ts
+++ server/app/Models/User.ts
@@ -1,14 +1,22 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
-import {
-  column,
-  beforeSave,
-  BaseModel,
-} from '@ioc:Adonis/Lucid/Orm'
+import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column({ serializeAs: 'firstName' })
+  public firstName: string
+
+  @column({ serializeAs: 'lastName' })
+  public lastName: string
+
+  @column({ serializeAs: 'phoneNumber' })
+  public phoneNumber: string
+
+  @column({ serializeAs: 'nationalId' })
+  public nationalId: string
 
   @column()
   public email: string
@@ -26,7 +34,7 @@ export default class User extends BaseModel {
   public updatedAt: DateTime
 
   @beforeSave()
-  public static async hashPassword (user: User) {
+  public static async hashPassword(user: User) {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password)
     }
--- server/database/migrations/1587988332388_users.ts
+++ server/database/migrations/1587988332388_users.ts
@@ -3,17 +3,21 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class UsersSchema extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
+      table.string('phone_number').nullable().unique()
+      table.string('national_id').nullable().unique()
       table.timestamps(true)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
