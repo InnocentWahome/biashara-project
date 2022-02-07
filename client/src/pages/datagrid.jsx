e61ef8621911b0164f0624dc597a9371dd1e1587
InnocentWahome <innocentwahome@gmail.com>
2022-02-07 13:28:05 +0300

    feat: custom columns datagrid

--- client/src/pages/datagrid.jsx
+++ client/src/pages/datagrid.jsx
@@ -1,30 +1,42 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
 import { DataGrid } from "@mui/x-data-grid"
-import { useDemoData } from "@mui/x-data-grid-generator"
 import PageLayout from "../layouts/PageLayout"
+// import axios from "axios"
 
-export default function PageSizeControlled() {
-  const { data } = useDemoData({
-    dataSet: "Commodity",
-    rowLength: 500,
-    maxColumns: 6,
-  })
+const columns = [
+  { field: "id", headerName: "ID" },
+  { field: "title", headerName: "Title", width: 300 },
+  { field: "body", headerName: "Body", width: 600 },
+]
 
+const DataTable = () => {
+  const [tableData, setTableData] = useState([])
   const [pageSize, setPageSize] = React.useState(25)
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(data => data.json())
+      .then(data => setTableData(data))
+  }, [])
+
+  console.log(tableData)
 
   return (
     <PageLayout>
       <div class="container">
-        <div style={{ height: 600, width: "70%" }}>
+        <div style={{ height: 700, width: "200" }}>
           <DataGrid
+            rows={tableData}
             pageSize={pageSize}
             onPageSizeChange={newPage => setPageSize(newPage)}
             pagination
-            {...data}
-            editable
+            columns={columns}
+            checkboxSelection
           />
         </div>
       </div>
     </PageLayout>
   )
 }
+
+export default DataTable
