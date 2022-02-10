6825336761f519324eca693af1465bd571934444
InnocentWahome <innocentwahome@gmail.com>
2022-02-10 22:24:53 +0300

    add zip file, fix: order migration err

--- server/database/migrations/1644518064502_orders.ts
+++ server/database/migrations/1644518064502_orders.ts
@@ -6,14 +6,14 @@ export default class Orders extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('productId')
-      table.string('productName')
-      table.string('userId')
+      table.string('product_id')
+      table.string('product_name')
+      table.string('user_id')
       table.integer('cost')
       table.integer('quantity')
-      table.boolean('adminApproval')
-      table.boolean('dispatchStatus')
-      table.boolean('deliveryStatus')
+      table.boolean('admin_approval')
+      table.boolean('dispatch_status')
+      table.boolean('delivery_status')
       table.timestamps(true)
     })
   }
