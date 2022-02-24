5d3d0760d2c365c68f8b623ae80964b4db1fbddc
InnocentWahome <innocentwahome@gmail.com>
2022-02-24 18:52:30 +0300

    refactor project layout

--- server/database/seeders/Feedback.ts
+++
@@ -1,8 +0,0 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { FeedbackFactory } from 'Database/factories/FeedbackFactory'
-
-export default class FeedbackSeeder extends BaseSeeder {
-  public async run() {
-    await FeedbackFactory.createMany(10)
-  }
-}
--- server/database/seeders/Service.ts
+++
@@ -1,8 +0,0 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { ServiceFactory } from 'Database/factories/ServiceFactory'
-
-export default class ServiceSeeder extends BaseSeeder {
-  public async run() {
-    await ServiceFactory.createMany(10)
-  }
-}
